e07b6a26e7b7b50cbf3e4d941f62b987967417ba
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-12-07 20:54:08 +0100

    Wait for new messages instead of raising an exception because of an empty message queue

--- src/amqp/list_handler.clj
+++ src/amqp/list_handler.clj
@@ -7,6 +7,18 @@
         [amqp.status-handler]
         [amqp.aggregate-handler]))
 
+(defn wait-for-new-messages
+  [channel queue auto-ack]
+  (let [message (atom [])
+        _ (swap! message (constantly (lb/get channel queue auto-ack)))]
+    (loop [wait-for-15-more-seconds (nil? (second @message))]
+      (when wait-for-15-more-seconds
+        (log/info (str "About to wait for 15 seconds for new messages"))
+        (Thread/sleep (* 15 1000))
+        (swap! message (constantly (lb/get channel queue auto-ack)))
+        (recur (nil? (second @message)))))
+    @message))
+
 (defn pull-messages-from-lists-queue
   "Pull messages from a queue dedicated to status collection from lists"
   [options]
@@ -15,7 +27,9 @@
          channel :channel
          queue :queue
          entity-manager :entity-manager} options
-        [{:keys [delivery-tag]} payload] (lb/get channel queue auto-ack)
+        new-message (wait-for-new-messages channel queue auto-ack)
+        {:keys [delivery-tag]} (first new-message)
+        payload (second new-message)
         payload-body (json/read-str (php->clj (String. payload "UTF-8")))
         screen-name (get payload-body "screen_name")
         aggregate-id (get payload-body "aggregate_id")]
