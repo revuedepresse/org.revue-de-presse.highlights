f23a89115f33095521f08f532e28d10fab22fbd7
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-12-09 19:25:34 +0100

    Add start and end date of highlights refreshments

--- bin/refresh-highlights.sh
+++ bin/refresh-highlights.sh
@@ -2,10 +2,14 @@
 
 function refresh_highlights() {
     local lein_bin=/usr/local/bin/lein
-
-    /bin/bash -c "${lein_bin} run save-highlights `date -I` && \
-    ${lein_bin} run record-popularity-of-highlights `date -I`" 2>> ./logs/highlights.error.log \
+    local now="$(date)"
+    /bin/bash -c "echo 'Started to record popularity of tweets at "${now}"' && \
+    ${lein_bin} run save-highlights `date -I` && \
+    ${lein_bin} run record-popularity-of-highlights `date -I`" \
+    2>> ./logs/highlights.error.log \
     >> ./logs/highlights.out.log
+
+   echo 'Finished recording popularity of tweets at "'"$(date)"'"' >> ./logs/highlights.out.log
 }
 
 refresh_highlights
