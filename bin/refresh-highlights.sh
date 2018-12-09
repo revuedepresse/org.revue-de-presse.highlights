f80007014e1ea98e404a7663fa3c3cc43efe7f2b
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-12-09 17:43:40 +0100

    Add script to be run every hour to refresh highlights

--- bin/refresh-highlights.sh
+++ bin/refresh-highlights.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 function refresh_highlights() {
-    /bin/bash -c '/usr/local/bin/lein run save-highlights `date -I` && \
-    lein run record-popularity-of-highlights `date -I` ' 2>> ./logs/highlights.error.log \
+    local lein_bin=/usr/local/bin/lein
+
+    /bin/bash -c "${lein_bin} run save-highlights `date -I` && \
+    ${lein_bin} run record-popularity-of-highlights `date -I`" 2>> ./logs/highlights.error.log \
     >> ./logs/highlights.out.log
 }
 
