a7eadedf9eb3bfff36e4bc4a61c60d2de47e2188
Thierry Marianne <thierrymarianne@users.noreply.github.com>
2018-12-10 23:33:30 +0100

    Revise command to be executed regularly to refresh popularity of statuses (#35)

--- bin/refresh-highlights.sh
+++ bin/refresh-highlights.sh
@@ -5,11 +5,9 @@ function refresh_highlights() {
     local now="$(date)"
     /bin/bash -c "echo 'Started to record popularity of tweets at "${now}"' && \
     ${lein_bin} run save-highlights `date -I` && \
-    ${lein_bin} run record-popularity-of-highlights `date -I`" \
-    2>> ./logs/highlights.error.log \
-    >> ./logs/highlights.out.log
-
-   echo 'Finished recording popularity of tweets at "'"$(date)"'"' >> ./logs/highlights.out.log
+    ${lein_bin} run record-popularity-of-highlights `date -I` " 2>> ./logs/highlights.error.log >> ./logs/highlights.out.log && \
+    echo 'Finished recording popularity of tweets at "'"$(date)"'"' >> ./logs/highlights.out.log || \
+    echo 'Something went horribly horribly wrong' >> ./logs/highlights.out.log
 }
 
 refresh_highlights
