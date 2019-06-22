91116517b4fdb609e5403411f789149f0873c173
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-06-22 14:21:12 +0200

    Deprecate generation of timely statuses from props

--- src/command/collect_timely_statuses.clj
+++ src/command/collect_timely_statuses.clj
@@ -1,9 +1,7 @@
 (ns command.collect-timely-statuses
   (:require
-    [clj-time.core :as t]
     [clj-time.coerce :as c]
     [clojure.tools.logging :as log]
-    [clojure.edn :as edn]
     [environ.core :refer [env]])
   (:use
     [amqp.handling-errors]
@@ -16,77 +14,6 @@
     [twitter.date]
     [twitter.status]))
 
-(def ^:dynamic *generate-timely-statuses-enabled-logging* false)
-
-(defn assoc-time-range
-  [status]
-  (let [time-range (get-time-range (:publication-date-time status))]
-    (assoc status :time-range time-range)))
-
-(defn generate-timely-statuses-from-statuses-props
-  [{aggregate-name :aggregate-name
-    ids            :ids
-    models         :models} week year]
-  (let [statuses (find-timely-statuses-props-for-aggregate ids)
-        find #(find-timely-statuses-by-constraints % aggregate-name models)
-        filtered-statuses (filter-out-known-statuses find statuses)
-        statuses-props (pmap assoc-time-range filtered-statuses)
-        _ (log/info (str "About to generate timely statuses from " year " for \"" aggregate-name "\""))
-        new-timely-statuses (bulk-insert statuses-props models)
-        total-timely-statuses (count new-timely-statuses)]
-    (when *generate-timely-statuses-enabled-logging*
-      (doall (pmap #(log/info (str "A timely status has been added for member \""
-                                   (:member-name %) "\" (" (:id %) ")")) new-timely-statuses)))
-    (when (pos? total-timely-statuses)
-      (log/info (str total-timely-statuses " new timely statuses have been added for \""
-                     aggregate-name
-                     (if week
-                       (str "\" from week #" week " and year " year)
-                       (str "\" from year " year)))))
-    new-timely-statuses))
-
-(defn generate-timely-statuses-from-subscriptions-of-member
-  [{aggregate-id :aggregate-id
-    models       :models}]
-  (let [timely-statuses (find-missing-timely-statuses-from-aggregate aggregate-id)
-        statuses-props (pmap assoc-time-range timely-statuses)
-        new-timely-statuses (bulk-insert statuses-props models)
-        total-timely-statuses (count new-timely-statuses)]
-    (when *generate-timely-statuses-enabled-logging*
-      (doall (pmap #(log/info (str "A timely status has been added for member \""
-                                   (:member-name %) "\" (" (:id %) ")")) new-timely-statuses)))
-    (cond
-      (> total-timely-statuses 1)
-      (log/info (str total-timely-statuses " new timely statuses have been added for aggregate #" aggregate-id))
-      (= 1 total-timely-statuses)
-      (log/info (str total-timely-statuses " new timely status has been added for aggregate #" aggregate-id))
-      :else
-      (log/info (str "No new timely status has been added for aggregate #" aggregate-id)))
-    new-timely-statuses))
-
-(defn collect-timely-statuses
-  ([week year]
-   (let [press-aggregate-name (:press (edn/read-string (:aggregate env)))]
-     (collect-timely-statuses {:week           week
-                               :year           year
-                               :aggregate-name press-aggregate-name})))
-  ([{year           :year
-     week           :week
-     aggregate-id   :aggregate-id
-     aggregate-name :aggregate-name}]
-   (let [generation-params {:models (get-entity-manager (:database env))}
-         {total-timely-status :total-timely-statuses
-          statuses-ids        :statuses-ids} (get-timely-statuses-for-aggregate {:aggregate-id     aggregate-id
-                                                                                 :aggregate-name   aggregate-name
-                                                                                 :publication-week week
-                                                                                 :publication-year year})
-         _ (when *generate-timely-statuses-enabled-logging*
-             (log/info (str total-timely-status " potential timely statuses have be counted for year " year ".")))
-         timely-statuses-params (assoc generation-params :ids statuses-ids)
-         timely-statuses-params (assoc timely-statuses-params :aggregate-name aggregate-name)
-         statuses (generate-timely-statuses-from-statuses-props timely-statuses-params week year)]
-     statuses)))
-
 (declare build-relationships)
 
 (defn handle-list
@@ -96,18 +23,6 @@
     (profile #(-> list-spec build-relationships))
     (catch Exception e
       (log/error (str "An error occurred with message " (.getMessage e))))))
-
-(defn consolidate-timely-statuses-from-subscriptions-for-member
-  [member]
-  (let [entity-manager (get-entity-manager (:database env))
-        aggregates (get-member-aggregates-by-screen-name member)
-        statuses (doall
-                   (pmap
-                     #(generate-timely-statuses-from-subscriptions-of-member
-                        {:aggregate-id (:aggregate-id %)
-                         :models       entity-manager})
-                     aggregates))]
-    statuses))
 
 (defn parse-status-publication-date
   [aggregate]
--- src/repository/timely_status.clj
+++ src/repository/timely_status.clj
@@ -1,7 +1,5 @@
 (ns repository.timely-status
   (:require [clojure.string :as string]
-            [clojure.tools.logging :as log]
-            [clj-uuid :as uuid]
             [korma.core :as db])
   (:use [korma.db]
         [repository.database-schema]
@@ -93,60 +91,6 @@
                                        :publication-week publication-week
                                        :publication-year publication-year
                                        :are-archived     are-archived})))
-
-(defn find-timely-statuses-props-for-aggregate
-  "Find the timely statuses properties for an aggregate"
-  ; Relies on original statuses
-  ([ids]
-   (let [bindings (take (count ids) (iterate (constantly "?") "?"))
-         more-bindings (string/join "," bindings)
-         params ids
-         query (str
-                 "SELECT DISTINCT                                          "
-                 "a.id as `aggregate-id`,                                  "
-                 "a.name as `aggregate-name`,                              "
-                 "s.ust_id as `status-id`,                                 "
-                 "s.ust_full_name as `member-name`,                        "
-                 "s.ust_created_at as `publication-date-time`              "
-                 "FROM weaving_status s                                    "
-                 "INNER JOIN weaving_status_aggregate sa                   "
-                 "ON sa.status_id = s.ust_id                               "
-                 "INNER JOIN weaving_aggregate a                           "
-                 "ON a.id = sa.aggregate_id                                "
-                 "WHERE (a.id, s.ust_id) NOT IN (                          "
-                 "  SELECT aggregate_id, status_id FROM timely_status      "
-                 ")                                                        ")
-         query (str query "AND s.ust_id IN (" more-bindings ")")
-         results (db/exec-raw [query params] :results)]
-     results)))
-
-(defn find-missing-timely-statuses-from-aggregate
-  "Find the statuses of a member published on a given day"
-  ([aggregate-id]
-   (let [query (str
-                 "SELECT                                                            "
-                 "sa.aggregate_id as `aggregate-id`,                                "
-                 "a.name as `aggregate-name`,                                       "
-                 "sa.status_id as `status-id`,                                      "
-                 "s.ust_full_name as `member-name`,                                 "
-                 "s.ust_created_at as `publication-date-time`                       "
-                 "FROM weaving_status_aggregate sa                                  "
-                 "INNER JOIN weaving_aggregate a                                    "
-                 "ON (                                                              "
-                 "  sa.aggregate_id = a.id                                          "
-                 "  AND a.id = ?                                                    "
-                 ")                                                                 "
-                 "INNER JOIN weaving_status s                                       "
-                 "ON (                                                              "
-                 "    s.ust_id = sa.status_id                                       "
-                 "    AND s.ust_full_name = a.screen_name                           "
-                 ")                                                                 "
-                 "WHERE (sa.status_id, sa.aggregate_id) NOT IN (                    "
-                 "    SELECT COALESCE(status_id, 0), COALESCE(aggregate_id, 0)      "
-                 "     FROM timely_status                                           "
-                 ")                                                                 ")
-         results (db/exec-raw [query [aggregate-id]] :results)]
-     results)))
 
 (defn find-aggregate-having-publication-from-date
   "Find the distinct aggregates for which publications have been collected for a given date"
