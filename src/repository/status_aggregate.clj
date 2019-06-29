73982917fdc558dc0d8de6fe7332832ab104a4bc
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-06-29 23:48:23 +0200

    Fix list of statuses found by aggregate

--- src/repository/status_aggregate.clj
+++ src/repository/status_aggregate.clj
@@ -22,16 +22,21 @@
         aggregate-screen-name-col (get-column "screen_name" aggregate-model)
         aggregate-list-id-col (get-column "list_id" aggregate-model)
         status-id-col (get-column "ust_id" status-model)
+        status-twitter-id-col (get-column "ust_status_id" status-model)
         text-col (get-column "ust_text" status-model)
+        created-at-col (get-column "ust_created_at" status-model)
         status-api-document (get-column "ust_api_document" status-model)]
     (->
       (db/select* model)
       (db/fields [:status_id :status-id]
                  [:aggregate_id :aggregate-id]
                  [status-api-document :status-api-document]
-                 [text-col :status-api-document]
-                 [aggregate-name-col :aggregate-name-col]
+                 [text-col :text]
+                 [created-at-col :created-at]
+                 [status-twitter-id-col :status-twitter-id]
+                 [aggregate-name-col :aggregate-name]
                  [aggregate-screen-name-col :aggregate-screen-name-col]
+                 [aggregate-screen-name-col :screen-name]
                  [aggregate-list-id-col :aggregate-list-id-col])
       (db/join aggregate-model (= aggregate-id-col :aggregate_id))
       (db/join status-model (= status-id-col :status_id)))))
