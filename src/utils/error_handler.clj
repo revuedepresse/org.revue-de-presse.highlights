b8a4fd13e8a2b70f99a133789deb629ad9ff4c0c
Thierry Marianne <thierry@marianne.io>
2022-07-12 19:42:19 +0200

    Print stack trace in debug mode only

--- src/utils/error_handler.clj
+++ src/utils/error_handler.clj
@@ -1,12 +1,16 @@
 (ns utils.error-handler
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [environ.core :refer [env]]))
 
 (defn log-error
   [e & [prefix no-stack-trace]]
   (let [prefix (if (some? prefix)
                  prefix
-                 "")]
+                 "")
+        print-stack-trace (and
+                            (nil? no-stack-trace)
+                            (true? (:debug env)))]
     (log/error (str prefix (.getMessage e)))
-    (when-not no-stack-trace
+    (when print-stack-trace
       (doall
         (map println (.getStackTrace e))))))
