6a76f5b0b7320712aa8f7ccd930e52d4dfdf826d
Thierry Marianne <thierry@marianne.io>
2022-07-17 10:30:11 +0200

    Renamed function responsible for loading configuration parameters

--- fun.sh
+++ fun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-function _set_up_configuration_files() {
+function load_configuration_parameters() {
     if [ ! -e ./.env ]; then
         cp --verbose ./.env{.dist,}
     fi
@@ -52,7 +52,7 @@ function build() {
     local WORKER_UID
     local WORKER_GID
 
-    _set_up_configuration_files
+    load_configuration_parameters
 
     if [ $? -gt 1 ];
     then
@@ -150,7 +150,7 @@ function remove_running_container_and_image_in_debug_mode() {
     local WORKER_GID
     local WORKER
 
-    _set_up_configuration_files
+    load_configuration_parameters
 
     if [ $? -gt 1 ];
     then
@@ -227,7 +227,7 @@ function install() {
     local WORKER_GID
     local WORKER
 
-    _set_up_configuration_files
+    load_configuration_parameters
 
     if [ $? -gt 1 ];
     then
@@ -289,7 +289,7 @@ function start() {
     local WORKER_UID
     local WORKER_GID
 
-    _set_up_configuration_files
+    load_configuration_parameters
 
     if [ $? -gt 1 ];
     then
